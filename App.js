d889801ae3cd3e5a81ecd24f924c3c212cd97c70
raju-molla <iamraju.eu56@gmail.com>
2022-06-16 17:29:47 +0600

    add and delet

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import TodoList from './component/todo-list';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TodoList />
     </View>
   );
 }
